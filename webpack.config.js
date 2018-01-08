933efbd1500b0f4e43f70fc868167ca40933cfc2
黄涛(研发一部） <uid12403@sansi.com>
2018-01-08 15:48:45 +0800

    初步搭建webpack工程

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: path.resolve(__dirname, 'client/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[hash:8].js',
+        publicPath: '',//输出解析文件的目录，url相对于HTML页面
+    },
+    module: {
+        rules: [
+            {
+                test: /.jsx?$/,
+                include: path.resolve(__dirname, 'client'),
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'client/template.html'),
+            favicon: path.resolve(__dirname, 'client/favicon.ico')
+        })
+    ]
+}
